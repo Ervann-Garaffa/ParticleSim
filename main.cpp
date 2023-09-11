2c7f2b801e65e09eafe998cec31fbffe53ed6800
Ervann-Garaffa <ervanngaraffa-botta@hotmail.fr>
2023-09-11 11:51:40 +0200

    SFML window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
+	sf::RenderWindow window(sf::VideoMode(1000, 1000), "Hello");
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		window.display();
+	}
 
 	return EXIT_SUCCESS;
 }
