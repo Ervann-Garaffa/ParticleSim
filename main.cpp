ed1d3a046fdbd54edf69443754ac1b05e8363dc6
Ervann-Garaffa <ervanngaraffa-botta@hotmail.fr>
2023-09-11 21:28:25 +0200

    Main Grid array of pointers to Vectors of Particles + stored gridSub positions. Temp Vector of Particles. != overload.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ const int WINDOW_HEIGHT = 1000;
 const float TAR_FPS = 144.1;
 const float TAR_DT = 1000000 / TAR_FPS;
 const float G = 9.81 / TAR_FPS;
-
 const float REBOUND_EFFICIENCY = 0.9;
+const int EDGE = 5;
+const int SIZE = EDGE * EDGE;
+const float GRID_SUB_WIDTH = WINDOW_WIDTH / EDGE;
+const float GRID_SUB_HEIGHT = WINDOW_HEIGHT / EDGE;
 
 // Random generator + function
 std::random_device rd;
@@ -24,9 +27,11 @@ struct Particle {
 	float m_rad;
 	sf::Vector2f m_pos, m_vel, m_acc;
 	sf::CircleShape m_body;
+	int m_gridX, m_gridY; // Grid Subdivision memory stored
 
 	Particle(float rad, float posX, float posY, float velX, float velY, float accX, float accY)
-		: m_rad(rad), m_pos(posX, posY), m_vel(velX, velY), m_acc(accX, accY)
+	: m_rad(rad), m_pos(posX, posY), m_vel(velX, velY), m_acc(accX, accY), 
+	  m_gridX(posX/GRID_SUB_WIDTH), m_gridY(posY/GRID_SUB_HEIGHT)
 	{
 		m_body.setOrigin(m_rad, m_rad);
 		m_body.setFillColor(sf::Color::Red);
@@ -37,6 +42,24 @@ struct Particle {
 	void resetForces() {
 		m_acc.x = 0;
 		m_acc.y = G;
+	}
+
+	bool operator!=(const Particle &other) const {
+		if (this->m_rad != other.m_rad ||
+			this->m_pos != other.m_pos ||
+			this->m_vel != other.m_vel)
+			return true;
+		return false;
+	}
+
+	bool changedGridSub() {
+		int newGridX = m_pos.x / GRID_SUB_WIDTH;
+		int newGridY = m_pos.y / GRID_SUB_HEIGHT;
+
+		if (newGridX != m_gridX || newGridY != m_gridY)
+			return true; // Grid subdivision has changed
+
+		return false; // Grid subdivision has not changed
 	}
 
 	void containerCollisions() {
@@ -83,26 +106,34 @@ int main() {
 		return -42;
 
 	sf::Clock simClock;
-
+	
 	sf::Text fpsCounter;
 	fpsCounter.setFillColor(sf::Color::White);
 	fpsCounter.setFont(displayFont);
 	fpsCounter.setCharacterSize(15);
 	fpsCounter.setPosition(20, 20);
 
-	Particle part(15, 500, 50, 0, 0, 0, 0);
+	std::vector<Particle>** grid = new std::vector<Particle>*[SIZE];
+	std::vector<Particle> temp;
 
 	while (window.isOpen()) {
 		simClock.restart();
 		
 		sf::Event event;
-		while (window.pollEvent(event))
+		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::MouseButtonPressed) {
+				temp.emplace_back(rand(1, 20), event.mouseButton.x, event.mouseButton.y, 0, 0, 0, 0);
+			}
+		}
 
 		window.clear();
-		part.update();
-		part.render(window);
+
+		for (auto &p : temp) {
+			p.update();
+			p.render(window);
+		}
 
 		while (simClock.getElapsedTime().asMicroseconds() < TAR_DT) {}
 		fpsCounter.setString(std::to_string(1000000 / simClock.restart().asMicroseconds()).append(" FPS"));
